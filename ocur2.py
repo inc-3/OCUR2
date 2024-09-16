eaaa1b1984673d9875763bb7e6a11949d277a495
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-09-16 22:30:16 +0600

    Update ocur2.py

--- ocur2.py
+++ ocur2.py
@@ -4,8 +4,8 @@ import threading
 import time
 import re
 from bs4 import BeautifulSoup
-from BD import common_bangladeshi_names
-from keywords import indian_keywords
+from BD import bdn
+from IN import inn
 
 # COLOURS
 GREEN = "\33[38;5;46m"
@@ -16,7 +16,7 @@ BLUE = "\33[1;97m"
 CYAN = "\33[1;97m"
 X = f"{WHITE}[\33[1;91m~{WHITE}]"
 
-# LINE
+# LinnE
 line = "\33[1;97m═" * 40
 logo = f"""
 {WHITE}
@@ -75,7 +75,7 @@ def is_bangladeshi(name):
     # Define a regex pattern for Bengali characters
     bengali_pattern = re.compile("[\u0980-\u09FF]")
     # Check if the name contains Bengali characters or is in the common Bangladeshi names set
-    return bool(bengali_pattern.search(name)) or any(common_name in name for common_name in common_bangladeshi_names)
+    return bool(bengali_pattern.search(name)) or any(common_name in name for common_name in bdn)
 
 # Function to filter out non-Bangladeshi names
 def filter_bangladeshi_names(data):
@@ -108,7 +108,12 @@ def save_data_to_file(data, file_path):
 def contains_arabic(text):
     arabic_re = re.compile(r'[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]')
     return bool(arabic_re.search(text))
-
+       
+def contains_hindi(text):
+    hindi_re = re.compile(r'[\u0900-\u097F]')
+    return bool(hindi_re.search(text))    
+ 
+ 
 def remove_duplicates_by_uid(data):
     print("Removing duplicates UID...")
     seen_uids = set()
@@ -120,17 +125,36 @@ def remove_duplicates_by_uid(data):
             filtered_data.append(entry)
     return filtered_data
 
-def filter_names(data, indian_keywords):
+def filter_names(data, inn):
     print("Removing Indian Uid...")
     filtered_data = []
     for entry in data:
         uid, name = entry.split('|', 1)
         
-        # Check if any part contains Indian keywords or Arabic characters
-        if not any(any(keyword in part for keyword in indian_keywords) or contains_arabic(part) for part in name.strip().split()):
+        # Check if any part contains Indian IN or Arabic characters
+        if not any(any(keyword in part for keyword in inn) or contains_arabic(part) or contains_hindi(part) for part in name.strip().split()):
             filtered_data.append(entry)
     
     return filtered_data
+    
+    
+def rm_bd(data, bdn):
+    print("Removing Bangladeshi Uid...")
+    filtered_data = []
+    
+    for entry in data:
+        if '|' in entry:
+            uid, name = entry.split('|', 1)
+            
+            # If the name is NOT a common Bangladeshi name, keep it
+            if not any(common_name in name for common_name in bdn):
+                filtered_data.append(entry)
+        else:
+            print(f"Ignoring line: {entry} (Does not contain expected format)")
+    
+    return filtered_data
+
+
 
 def sort_data_lexicographically_desc(data):
     return sorted(data, reverse=True)
@@ -141,7 +165,7 @@ def rm(data):
         if '|' in entry:  # Check if the entry contains the expected delimiter
             number, name = entry.split('|', 1)
             name_parts = name.split()
-            if len(name_parts) > 0 and name_parts[0] in ["Md", "Mst"]:
+            if len(name_parts) > 0 and name_parts[0] in ["Md",  "MD", "Sk", "Md.",  "Mst"]:
                 name = " ".join(name_parts[1:])  # Remove 'Md' or 'Mst' if it's the first name
             updated_data.append(f"{number}|{name}")
         else:
@@ -193,7 +217,7 @@ def method_2():
         else:
             data = read_data_from_file(file_path)
             data = remove_duplicates_by_uid(data)
-            filtered_data = filter_names(data, indian_keywords)
+            filtered_data = filter_names(data, inn)
             sorted_data = sort_data_lexicographically_desc(filtered_data)
             save_data_to_file(sorted_data, file_path)
             print(f"\rProcessing completed Remaining Uid: {GREEN}{len(filtered_data)}{reset_text}\n")
@@ -230,6 +254,24 @@ def dup():
         input("Press Enter to go back to the main menu...")
         main()  # Go back to the main menu    
 
+def chk():
+    clear_screen_and_print_logo()
+    file_paths = input_file_path()
+    if isinstance(file_paths, str):  # If a single custom file path is provided
+        file_paths = [file_paths]
+    
+    not_found_files = []
+    
+    for file_path in file_paths:
+        if not os.path.exists(file_path):
+            not_found_files.append(file_path)
+        else:
+            data = read_data_from_file(file_path)
+            data = remove_duplicates_by_uid(data)
+            filtered_data = rm_bd(data, bdn)
+            sorted_data = sort_data_lexicographically_desc(filtered_data)
+            save_data_to_file(sorted_data, file_path)
+            print(f"\rProcessing completed Remaining Uid: {GREEN}{len(filtered_data)}{reset_text}\n")
 
 def main():
     sys.stdout.write('\x1b]2; INCEPTION \x07')
@@ -237,7 +279,8 @@ def main():
 
     print('[1] Save Only Bangladeshi Uid')
     print('[2] Remove Indian Uid')
-    print('[3] Remove Duplicate Uid\n')
+    print('[3] Remove Duplicate Uid')
+    print('[4] Remove Bangladeshi uid\n')
 
     method_choice = input('[?] Your Choice : ').strip()
 
@@ -246,7 +289,9 @@ def main():
     elif method_choice == '2':
         method_2()
     elif method_choice == '3':
-        dup()    
+        dup()
+    elif method_choice == '4':
+        chk()
     else:
         print("Invalid choice. Please choose 1 or 2.")
 
