1e922c06fc396d7ea99f1836097058891259cdba
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-09-19 19:28:02 +0600

    Update BDM.py

--- BDM.py
+++ BDM.py
@@ -1,4 +1,1 @@
-bdn = {
-    # Male names
-    "Md", "Md.", "Abdul", "Akhtar", "Ali", "Amin", "Anis", "Asif", "Aziz", "Bashir", "Bilal"
-}
+bdn = {"Md", "Md.", "Abdul", "Akhtar", "Ali", "Amin", "Anis", "Asif", "Aziz", "Bashir", "Bilal"}
