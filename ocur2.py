26e5cd44008cea39b9f3c41ba55f5dd4a773a000
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-09-27 01:01:31 +0600

    Update ocur2.py

--- ocur2.py
+++ ocur2.py
@@ -177,7 +177,7 @@ def clear_screen_and_print_logo():
     os.system("clear")  # Clear the screen
     print(logo)         # Print the logo
 
-def method_5():
+def method_1():
     clear_screen_and_print_logo()
     file_paths = input_file_path()
     
@@ -191,10 +191,11 @@ def method_5():
             not_found_files.append(file_path)
         else:
             data = read_data_from_file(file_path)
-            # Filter lines containing 'Md', 'MD', or 'Md.'
-            md_lines = [line for line in data if any(prefix in line for prefix in ['Md', 'MD', 'Md.'])]
-            save_data_to_file(md_lines, file_path)
-            print(f"\rProcessing completed. Remaining Uid: {GREEN}{len(md_lines)}{reset_text}\n")
+            data = remove_duplicates_by_uid(data)
+            filtered_data = filter_bangladeshi_names(data, bdn)
+            sorted_data = sort_data_lexicographically_desc(filtered_data)
+            save_data_to_file(sorted_data, file_path)
+            print(f"\rProcessing completed Remaining Uid: {GREEN}{len(filtered_data)}{reset_text}\n")
     
     if not_found_files:
         print("The following files were not found:")
